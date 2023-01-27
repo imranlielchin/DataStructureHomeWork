e816ae1f6641e1b54a001d7399bf0da0e06e5284
imranlielchin <elchinii@code.edu.az>
2023-01-27 12:21:14 +0400

    init

---
+++ DataStructureHW/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureHW
+{
+    internal class Book
+    {
+        string Name;
+        string AuthorName;   
+        int PublishedDate;
+        int PageCount;
+        
+        public Book(string name, string author, int page, int date)
+        { 
+            this.Name = name;
+            this.PublishedDate = date;
+            this.AuthorName = author;   
+            this.PageCount = page;
+        }   
+        public override string ToString() 
+        {
+            return ($"{AuthorName} \"{Name}\" {PublishedDate}");
+        }
+        
+        public void ReturnFullInfo()
+        {
+            Console.WriteLine($"Eserin adi:{Name}\nMuellifin adi:{AuthorName}\nNesr ili:{PublishedDate}\nSehife sayi:{PageCount}");
+
+        }
+
+
+
+
+    }
+    
+
+}
---
+++ DataStructureHW/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DataStructureHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            Book book1 = new Book("Qerb burkusu", "Cingiz Abdullayev", 300,2013);
+            Book book2 = new Book("Sefiller", "Viktor Huqo", 1174, 1862);
+            Book book3 = new Book("Cinayet ve Ceza", "Dostoyevski", 845, 1866);
+            Book[] books = new Book[] { book1, book2, book3 };
+            foreach (Book book in books)
+            {
+                Console.WriteLine($"{book}\n---");
+            }
+
+            #endregion
+            #region Task 2
+            book1.ReturnFullInfo();
+            #endregion
+
+
+
+
+
+
+
+        }
+    }
+}
